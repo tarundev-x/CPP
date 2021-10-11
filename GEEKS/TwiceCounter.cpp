68583574215506991a9f76f009fa3abb82a4ee23
Tarun Dev <62273965+tarundev-x@users.noreply.github.com>
2021-10-11 21:02:38 +0530

    Create TwiceCounter.cpp

---
+++ GEEKS/TwiceCounter.cpp
@@ -0,0 +1,16 @@
+ int countWords(string list[], int n)
+        {
+           //code here.
+         
+          unordered_map<string,int>mp;
+          for(int i=0;i<n;i++)
+          {
+              mp[list[i]]++;
+          }
+          int count=0;
+          for(auto it:mp)
+          {
+              if(it.second==2) count++;
+          }
+          return count;
+        }
