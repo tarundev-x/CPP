4725aedb84bc9d96c73e56f9d0f07834fb4af08b
Tarun Dev <62273965+tarundev-x@users.noreply.github.com>
2021-12-25 16:22:13 +0530

    Create validParanthesis.cpp

---
+++ LOGIC/validParanthesis.cpp
@@ -0,0 +1,27 @@
+bool isBalanced(string s)
+{
+    stack<char>st;
+    for(char x:s)
+    {
+        if(x=='('||x=='{'||x=='[')
+        {
+            st.push(x);
+        }
+        else
+        {
+            if(st.empty()==false)
+            {
+                return false;
+            }
+            if(matching(st.top(),x)==false)
+            {
+                return false;
+            }
+            else
+            {
+                st.pop();
+            }
+        }
+    }
+    return (st.empty()==true);
+}
