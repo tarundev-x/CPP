2383d30c3e2b42219b3c0b140524877b29325c0c
Tarun Dev <62273965+tarundev-x@users.noreply.github.com>
2022-01-13 18:40:19 +0530

    Create sumdigit.cpp

---
+++ Recursion/sumdigit.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int sum=0;
+int sumd(int str)
+{
+	if(str==0) return 0;
+	
+	return sumd(str/10)+str%10;
+	
+
+}
+int main()
+{
+	int str;
+	cin>>str;
+	cout<<sumd(str);
+}
